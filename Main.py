afcf1f84ce2a4ad6d6f15412b27d9793363090e1
bahouyassine <bahouyassine7@gmail.com>
2023-08-21 20:17:59 +0200

    create df

--- Main.py
+++ Main.py
@@ -1,13 +1,7 @@
 from EbayScraper import EbayScraper
 
-# Create an instance of the EbayScraper class
 scraper = EbayScraper()
-
-
-# Call the search_price method with the product you want to search
-search_term = "dell latitude E7450 i5"
+search_term = "Dell Ultrabook E7450 Core "
 num_pages = 5
-
-
-# Call the _save_to_file method
 items = scraper._get_items(f"{scraper.base_url}?_nkw={search_term.replace(' ', '+')}&_pgn={num_pages}")
+df = scraper.dataFrame
